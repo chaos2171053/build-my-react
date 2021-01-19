9e77d8da251a43092efb998791407439417a2b34
chaos2171053 <chaos2171053@gmail.com>
2021-01-19 14:34:54 +0800

    add:performUnitOfWork

--- src/Chaos.js
+++ src/Chaos.js
@@ -53,10 +53,57 @@ function workLoop(deadline) {
   requestIdleCallback(workLoop);
 }
 
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO add dom node
-  // TODO create new fibers
-  // TODO return next unit of work
+function performUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    // fiber 与 第一个子节点建立连接
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    // 第一个子节点是其他子节点的兄弟节点
+    prevSibling = newFiber;
+    index++;
+  }
+  //  return next unit of work
+
+  // 如果有子节点，则把第一个子节点作为下一个 unit work
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+
+  while (nextFiber) {
+    // 如果有兄弟节点，则把兄弟节点作为下一个 unit work
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    // 否则，向上寻找兄弟节点
+    nextFiber = nextFiber.parent;
+  }
 }
 
 function createDom(fiber) {

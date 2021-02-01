ebd5f9c682dc7e8d59145d82952dbf809e9c9c05
chaos2171053 <chaos2171053@gmail.com>
2021-02-01 16:07:52 +0800

    add:react.memo

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import ReactChildrenDemo from './demo/ReactChildrenDemo.jsx';
+// import ReactChildrenDemo from './demo/ReactChildrenDemo.jsx';
+import ReactMemoDemo from './demo/ReactMemoDemo.jsx';
 
 export default function App() {
   return (
     <>
-      <ReactChildrenDemo />
+      {/* <ReactChildrenDemo /> */}
+      <ReactMemoDemo />
     </>
   );
 }
